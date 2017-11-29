95a0682f5271e4749ee643ab84a523e03e4c5cf1
Aran Cardona <a15aracarjim@iam.cat>
2017-11-29 10:35:47 +0100

    Proves skeleton en el php calendari

--- web/php/calendari.php
+++ web/php/calendari.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 
- <html class="">
+ <html>
  <link rel="stylesheet" href="../css/myitvdesign.css">
  <link rel="stylesheet" href="../lib/normalize.css">
  <link rel="stylesheet" href="../lib/skeleton.css">
  <script src="../lib/jquery.js"></script>
 <head>
 <meta charset="utf-8" />
-<title>Calendari</title>
-<head>Calendari</head>
-<h1>Calendari</h1>
+<title>IAMotors</title>
+</head>
+<body>
+	<div class="container">
+  		<div class="row">
+    		<div class="three columns"></div>
+    		<div class="six columns">
+    			<table>
+    				<tr>
+    					<td>hola</td>
+    				</tr>
+    			</table>
+    		</div>
+    		<div class="three columns"></div>
+  		</div>
+  	</div>
+</body>
 </html>
