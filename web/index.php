4eb57397880a38d8af7f199f53fabbb624539cc9
Joan <a15joacalsas@iam.cat>
2017-12-01 11:55:13 +0100

    Index with JavaScript UPDATE

--- web/index.php
+++ web/index.php
@@ -5,30 +5,9 @@
  <link rel="stylesheet" href="lib/normalize.css">
  <link rel="stylesheet" href="lib/skeleton.css">
  <script src="lib/jquery.js"></script>
+ <script src="js/functionsIndex.js"></script>
 <script>
-$(document).ready(function() {
-	$("#matricula").focus();
-var error=0;
-	$('#matricula').focusout(function () {
-var pattern = /^[A-Z]{0,2}\d\d\d\d[A-Z]{2,3}$/ ;
-if (!pattern.test($('#matricula').val())) {
-error=1;
-$('#matricula').css("background-color","#ff5050");
-}else
-{
-	$('#matricula').css("background-color","white");
 
-}
-});
-
-$("#submit").click(function(event){
-	if(error==1){
-    event.preventDefault();
-    alert("Corrige la matricula");
-     }
- });
-
-});
 </script>
 <meta charset="utf-8" />
 
