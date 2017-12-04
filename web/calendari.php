75041d6a0c988be1ddbc02f2f939315b09f295db
Aran Cardona <a15aracarjim@iam.cat>
2017-12-04 14:18:41 +0100

    calendari.php pot obtenir les dades de la pàgina anterior

--- web/calendari.php
+++ web/calendari.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 
  <html>
+<head>
  <link rel="stylesheet" href="lib/normalize.css">
  <link rel="stylesheet" href="lib/skeleton.css">
  <link rel="stylesheet" href="css/myitvdesign.css">
  <script type="text/javascript" language="javascript" src="js/functionsCalendari.js"></script>
  <script src="lib/jquery.js"></script>
-<head>
 <meta charset="utf-8" />
 <title>IAMotors</title>
 </head>
@@ -18,10 +18,10 @@
     		<div class="six columns">
           <div id="calendari"></div>
           <?php 
-          if($_POST['date']){ 
-            $day = substr($_POST['date'],8,9);
-            $month = substr($_POST['date'],5,6);
-            $year = substr($_POST['date'],0,4);
+          if($_POST['dia']){ 
+            $day = substr($_POST['dia'],8,9);
+            $month = substr($_POST['dia'],6,-2);
+            $year = substr($_POST['dia'],0,4);
           } else {
             $day = date("d");
             $month = date("m");
@@ -30,10 +30,9 @@
           
            ?>
     			<script language="javascript">
-            var dd =  <?php echo $day; ?>;
-            console.log(dd);
-            var mm = <?php echo $month; ?>; 
-            var yyyy = <?php echo $year; ?>;
+            var dd = <?php echo $day ?>;
+            var mm = <?php echo $month ?>;
+            var yyyy = <?php echo $year ?>;
 
             var datos = {
               dia : dd,
@@ -48,5 +47,15 @@
   		</div>
   	</div>
     <div id="footer"><?php include "footer.php" ?></div>
+    <?php
+      if($_POST){
+        $id = $_POST['id'];
+        $matricula = $_POST['matricula'];
+        $dia = $_POST['dia'];
+        $hora = $_POST['hora'];
+        $email = $_POST['email'];
+        echo $id.' '.$matricula.' '.$dia.' '.$hora.' '.$email;
+      }
+    ?>
 </body>
 </html>
