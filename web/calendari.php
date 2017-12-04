4b934ce04732b2c908eaeade0e8f4958d8e8e51b
Aran Cardona <a15aracarjim@iam.cat>
2017-12-04 13:04:16 +0100

    afegit part php per quan rebi una data via post

--- web/calendari.php
+++ web/calendari.php
@@ -16,11 +16,23 @@
     		<div class="three columns"><p></p></div>
     		<div class="six columns">
           <div id="calendari"></div>
+          <?php 
+          if($_POST['date']){ 
+            $day = substr($_POST['date'],8,9);
+            $month = substr($_POST['date'],5,6);
+            $year = substr($_POST['date'],0,4);
+          } else {
+            $day = date("d");
+            $month = date("m");
+            $year = date("Y");
+          }
+          
+           ?>
     			<script language="javascript">
-            var today = new Date();
-            var dd = today.getDate();
-            var mm = today.getMonth()+1; 
-            var yyyy = today.getFullYear();
+            var dd =  <?php echo $day; ?> /*|| today.getDate()*/;
+            console.log(dd);
+            var mm = <?php echo $month; ?> /*|| today.getMonth()+1*/; 
+            var yyyy = <?php echo $year; ?> /*|| today.getFullYear()*/;
 
             var datos = {
               dia : dd,
