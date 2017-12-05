98d58d1328604761f17ef3f134f7147448221fd0
Miquel Vidal <a15miqvidgom@iam.cat>
2017-12-05 12:08:37 +0100

    admin site .htaccess

---
+++ web/admin/dashboard.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="../css/myitvdesign.css">
+	<link rel="stylesheet" href="../lib/normalize.css">
+	<link rel="stylesheet" href="../lib/skeleton.css">
+	<script src="../lib/jquery.js"></script>
+	<title>Dashboard</title>
+<head>
+<h1>Escriptori_admin</h1>
+</html>
