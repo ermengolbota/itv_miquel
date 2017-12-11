22963b72f2d8d781b06d04ca5f4f01b732076d9f
Aran Cardona <a15aracarjim@iam.cat>
2017-12-11 14:16:01 +0100

    començat disseny en el index.php

--- web/header.php
+++ web/header.php
@@ -1,6 +1,6 @@
 <?php
 	echo "<header>";
-		echo "<a href='./'><img class='logoHeader' src='images/IAM_CAT_logos_transparent_rgb_3.png'/></a>";
-		echo "<h1 class='titleHeader'>IAMotors</h1>";
+		echo "<div class='leftH'><a href='./'><img class='logoHeader' src='images/IAM_CAT_logos_transparent_rgb_3.png'/></a></div>";
+		echo "<div class='rightH'><h1 class='titleHeader'>IAMotors</h1></div>";
 	echo "</header>";
 ?>
--- web/index.php
+++ web/index.php
@@ -2,9 +2,9 @@
 
  <html>
  <head>
- <link rel="stylesheet" href="css/myitvdesign.css">
  <link rel="stylesheet" href="lib/normalize.css">
  <link rel="stylesheet" href="lib/skeleton.css">
+ <link rel="stylesheet" href="css/myitvdesign.css">
  <script src="lib/jquery.js"></script>
  <script src="js/functionsIndex.js"></script>
 <meta charset="utf-8" />
@@ -21,15 +21,20 @@ $_SESSION['block'] = 0;
         echo "<p style='color: red'>Tienes que rellenar la matricula";
      }
 ?>
-    <?php include "header.php" ?>
-	<h1>Benvingut a IAMotors</h1>
-<form action="php/index_comprova.php" method="POST">
-        Matricula:<input name="matricula" type="text" id="matricula" size="36" required="Tienes que rellenar este campo"><br />
-        <input type="submit" id="submit" value="Enviar">
-    </form>
+<div class="row">
+    <div class="two columns"><p></p></div>
+    <div class="eight columns">
+        <?php include "header.php" ?>
+    	<h1 class="titleIndex">Benvingut a IAMotors</h1>
+        <form action="php/index_comprova.php" method="POST">
+            <label class="form">Matricula: <input name="matricula" type="text" id="matricula" size="36" required="Tienes que rellenar este campo"></label>
+            <input type="submit" id="submit" value="Enviar">
+        </form>
+        <?php include "footer.php" ?>
+    </div>
+    <div class="two columns"></div>
 
-<div id="footer"><?php include "footer.php" ?></div>
-
+</div>
 
 </body>
 </html>
