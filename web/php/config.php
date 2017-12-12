5d2a995e618e838f3b2a6ad78845a167192cd7d3
Miquel Vidal <a15miqvidgom@iam.cat>
2017-12-12 14:16:35 +0100

    code comments, moved some functions to other files

--- web/php/config.php
+++ web/php/config.php
@@ -3,4 +3,11 @@
 	$db_database = 'IAMotors';
 	$db_username = 'root';
 	$db_password = 'root';
+
+	function connect() {
+		// posa les variables com a globals per a poder utilitzar-les
+		global $db_hostname, $db_database, $db_username, $db_password;
+		// retorna la connexio
+		return new mysqli($db_hostname, $db_username, $db_password, $db_database);
+	}
 ?>
--- web/php/gestioEdita.php
+++ web/php/gestioEdita.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	if ($_POST) {
+		// si s'arriba a la pàg amb el comportament predeterminat, converteix les dades a la sessió i redirecciona al calendari
 		$_SESSION["dia"] = $_POST["dia"];
 		$_SESSION["hora"] = $_POST["dia"];
 		$_SESSION["nom"] = $_POST["dia"];
@@ -10,6 +11,7 @@
 		$_SESSION["id"] = $_POST["id"];
 		header('Location: ../calendari.php');
 	} else {
+		// si no s'arriba a la pàg amb el comportament predeterminat, redirecciona a l'index
 		header('Location: ../');
 	}
 ?>
