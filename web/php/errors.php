97142c98f365cd5b81ca4bf3f2c088bd7614499e
Miquel Vidal <a15miqvidgom@iam.cat>
2017-12-13 11:56:03 +0100

    multiple errors handled in error.php

--- web/php/errors.php
+++ web/php/errors.php
@@ -2,7 +2,7 @@
 	// Aquest fitxer conté els errors que es mostren en error.php
 	$error_text = array(
 		"Alguna cosa ha fallat, canvis no realitzats. Si us plau començi de nou el procès.",
-		"otro error",
+		"Error de connexió amb la base de dades.",
 		);
 
 	function getErrorText($error_code) {
