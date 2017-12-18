fda59f614d35994beec19aa354cf1b21f2a9f143
Miquel Vidal <a15miqvidgom@iam.cat>
2017-12-18 14:13:49 +0100

    rm install.php, sql changed (a created database is nedded), deleted line in .htaccess

--- doc/sql/testData.sql
+++ doc/sql/testData.sql
@@ -1,12 +1,14 @@
-INSERT INTO IAMotors.Centre VALUES(1, 'Taller Principal');
+USE IAMotors;
 
-INSERT INTO IAMotors.Carril VALUES(1, 1);
-INSERT INTO IAMotors.Carril VALUES(2, 1);
+INSERT INTO Centre VALUES(1, 'Taller Principal');
 
-INSERT INTO IAMotors.Reserva VALUES('7777ISJ1512034603', '7777JSJ', CURRENT_DATE, TIME('17:00'), 1, 1, 'turismo', 'Arantxa', 'Cardona', '555666555', 'a15aracarjim@iam.cat');
-INSERT INTO IAMotors.Reserva VALUES('1111AAA1512034603', '1111BBB', CURRENT_DATE, TIME('17:00'), 2, 1, 'turismo', 'Arantxa', 'Cardona', '555666555', 'a15aracarjim@iam.cat');
-INSERT INTO IAMotors.Reserva VALUES('1112AAA1512034603', '1112BBB', CURRENT_DATE, TIME('18:00'), 2, 1, 'turismo', 'Arantxa', 'Cardona', '555666555', 'a15aracarjim@iam.cat');
-INSERT INTO IAMotors.Reserva VALUES('1113AAA1512034603', '1113BBB', CURRENT_DATE, TIME('17:30'), 1, 1, 'turismo', 'Joan', 'Calafat', '666555666', 'a15joacalsas@iam.cat');
-INSERT INTO IAMotors.Reserva VALUES('1114AAA1512034603', '1114BBB', CURRENT_DATE, TIME('18:00'), 1, 1, 'turismo', 'Joan', 'Calafat', '666555666', 'a15joacalsas@iam.cat');
-INSERT INTO IAMotors.Reserva VALUES('1115AAA1512034603', '1115BBB', CURRENT_DATE, TIME('19:00'), 1, 1, 'turismo', 'Miquel', 'Vidal', '666111666', 'a15miqvidgom@iam.cat');
-INSERT INTO IAMotors.Reserva VALUES('1116AAA1512034603', '1116BBB', CURRENT_DATE, TIME('19:00'), 2, 1, 'turismo', 'Miquel', 'Vidal', '666111666', 'a15miqvidgom@iam.cat');
+INSERT INTO Carril VALUES(1, 1);
+INSERT INTO Carril VALUES(2, 1);
+
+INSERT INTO Reserva VALUES('7777ISJ1512034603', '7777JSJ', CURRENT_DATE, TIME('17:00'), 1, 1, 'turismo', 'Arantxa', 'Cardona', '555666555', 'a15aracarjim@iam.cat');
+INSERT INTO Reserva VALUES('1111AAA1512034603', '1111BBB', CURRENT_DATE, TIME('17:00'), 2, 1, 'turismo', 'Arantxa', 'Cardona', '555666555', 'a15aracarjim@iam.cat');
+INSERT INTO Reserva VALUES('1112AAA1512034603', '1112BBB', CURRENT_DATE, TIME('18:00'), 2, 1, 'turismo', 'Arantxa', 'Cardona', '555666555', 'a15aracarjim@iam.cat');
+INSERT INTO Reserva VALUES('1113AAA1512034603', '1113BBB', CURRENT_DATE, TIME('17:30'), 1, 1, 'turismo', 'Joan', 'Calafat', '666555666', 'a15joacalsas@iam.cat');
+INSERT INTO Reserva VALUES('1114AAA1512034603', '1114BBB', CURRENT_DATE, TIME('18:00'), 1, 1, 'turismo', 'Joan', 'Calafat', '666555666', 'a15joacalsas@iam.cat');
+INSERT INTO Reserva VALUES('1115AAA1512034603', '1115BBB', CURRENT_DATE, TIME('19:00'), 1, 1, 'turismo', 'Miquel', 'Vidal', '666111666', 'a15miqvidgom@iam.cat');
+INSERT INTO Reserva VALUES('1116AAA1512034603', '1116BBB', CURRENT_DATE, TIME('19:00'), 2, 1, 'turismo', 'Miquel', 'Vidal', '666111666', 'a15miqvidgom@iam.cat');
