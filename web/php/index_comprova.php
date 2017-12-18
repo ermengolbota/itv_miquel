aa7bd7d307a59234b9252069bc1275abea843519
Miquel Vidal <a15miqvidgom@iam.cat>
2017-12-18 12:12:22 +0100

    deleted useless comments on index_comprova-php

--- web/php/index_comprova.php
+++ web/php/index_comprova.php
@@ -1,49 +1,39 @@
 <?php
- session_start();
- require_once 'config.php';
- require_once 'functions.php';
+	session_start();
+	require_once 'config.php';
+	require_once 'functions.php';
 
-// Create connection
-//$conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
- $conn = connect();
-// Check connection
-if ($conn->connect_error) {
-    //die("Connection failed: " . $conn->connect_error);
-    $_SESSION['error'] = 1;
-    header('Location: ../error.php');
-} 
+	// Create connection
+	$conn = connect();
+	// Check connection
+	if ($conn->connect_error) {
+		$_SESSION['error'] = 1;
+		header('Location: ../error.php');
+	} 
 
-//Es guarda la matricula passada per POST
-$matricula = $_POST['matricula'];
- 
-//Es guarda una variable de sessió de la matricula (servirà per passar la matricula entre els fitxers)
-$_SESSION['matricula']= $matricula;
+	//Es guarda la matricula passada per POST
+	$matricula = $_POST['matricula'];
+
+	//Es guarda una variable de sessió de la matricula (servirà per passar la matricula entre els fitxers)
+	$_SESSION['matricula']= $matricula;
 
 
-//Consulta que se li fa a la BBDD
-//$q = "SELECT * FROM Reserva WHERE matricula = '".$matricula."';";
-$q = "SELECT matricula FROM Reserva WHERE matricula = '$matricula' AND dia > CURDATE() OR matricula = '$matricula' AND dia = CURDATE() AND hora > CURTIME()";
+	//Consulta que se li fa a la BBDD
+	$q = "SELECT matricula FROM Reserva WHERE matricula = '$matricula' AND dia > CURDATE() OR matricula = '$matricula' AND dia = CURDATE() AND hora > CURTIME();";
 
-//Variable on es guarda la consulta
-//$result = $conn->query($q);
-$result = getResult($conn, $q);
+	//Variable on es guarda la consulta
+	$result = getResult($conn, $q);
 
-//Si la matricula existeix redirecciona a gestio.php, sino a calendari.php per crear la cita
-if ($result->num_rows > 0) {
+	//Si la matricula existeix redirecciona a gestio.php, sino a calendari.php per crear la cita
+	if ($result->num_rows > 0) {
+		close($conn);
+		header("Location: ../gestio.php");
+	} else {
+		close($conn);
+		$_SESSION['accio'] = "crear";
+		header("Location: ../calendari.php");
+	}
+
+	//Tanquem la connexio amb la BBDD
 	close($conn);
-	header("Location: ../gestio.php");
-
-    
-} else {
-	close($conn);
-	$_SESSION['accio'] = "crear";
-	header("Location: ../calendari.php");
-    
-    
-}
-
-
-//Tanquem la connexio amb la BBDD
-close($conn);
-
 ?>
