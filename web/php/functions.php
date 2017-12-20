4f32c248f735fe4ff1f256c938cb42529eff6937
Joan <a15joacalsas@iam.cat>
2017-12-20 13:16:28 +0100

    FIX } functions.php

--- web/php/functions.php
+++ web/php/functions.php
@@ -45,10 +45,10 @@
 			}
 		}
 		return $true;
-<<<<<<< HEAD
+
 	}	
-=======
-	}
+
+	
 
 	function validateNom(&$string) {
 		if (!empty($string) && is_string($string) && strlen($string)<=20) {
@@ -82,7 +82,4 @@
 		return false;
 	}
 
-
-
 ?>
->>>>>>> a786d7296ae6c966eb15bd914f76b1ebd41ce66f
